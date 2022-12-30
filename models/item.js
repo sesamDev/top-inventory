d24d24d8b8c48becbbe6358c1163d57544dfc8b1
Sebastian Hård Samils <sebastian.samils@gmail.com>
2022-12-30 13:29:54 +0100

    renamed file

--- models/item.js
+++ models/item.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
   name: { type: String, min: 3, max: 100, required: true },
+  manufacturer: { type: String, min: 2, max: 100, required: true },
   description: { type: String, min: 3, max: 100, required: true },
   inStock: { type: Number, min: 0, required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
 });
 
 ItemSchema.virtual("url").get(function () {
