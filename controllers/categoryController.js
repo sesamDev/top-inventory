6ceea3b545529b817333f3ac9c7fd53fa229660c
Sebastian Hård Samils <sebastian.samils@gmail.com>
2023-01-01 17:22:19 +0100

    boilerplate for category routes done.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -15,3 +15,43 @@ exports.category_list = (req, res, next) => {
       });
     });
 };
+
+// Display form for adding a new category
+exports.category_create_get = (req, res, next) => {
+  res.render("category_form", {
+    title: "Create new category",
+  });
+};
+
+// Handle creating category on POST
+exports.category_create_post = (req, res, next) => {
+  res.send("TO BE IMPLEMENTED");
+};
+
+// Display form for updating a new category
+exports.category_update_get = (req, res, next) => {
+  res.send("TO BE IMPLEMENTED");
+};
+
+// Handle update category on POST
+exports.category_update_post = (req, res, next) => {
+  res.send("TO BE IMPLEMENTED");
+};
+
+// Display category delete form on GET
+exports.category_delete_get = (req, res, next) => {
+  res.send("TO BE IMPLEMENTED");
+};
+
+// Handle category delete on post
+exports.category_delete_post = (req, res, next) => {
+  res.send("TO BE IMPLEMENTED");
+};
+
+// Display category details
+exports.category_detail = (req, res, next) => {
+  res.render("category_detail", {
+    title: "Will show category name here",
+    id: req.params.id,
+  });
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -5,10 +5,33 @@ const router = express.Router();
 const categoryController = require("../controllers/categoryController");
 const itemController = require("../controllers/itemController");
 
-/// ROUTES ///
+/// CATEGORY ROUTES ///
 
-// GET category list.
+// GET request for viewing category list.
 router.get("/category", categoryController.category_list);
+
+// GET request for creating a category.
+router.get("/category/create", categoryController.category_create_get);
+
+// POST request for creating a category.
+router.post("/category/create", categoryController.category_create_post);
+
+// GET request for updating a category.
+router.get("/category/:id/update", categoryController.category_update_get);
+
+// POST request for updating a category.
+router.post("/category/:id/update", categoryController.category_update_post);
+
+// GET request for deleting a category.
+router.get("/category/:id/delete", categoryController.category_delete_get);
+
+// POST request for deleting a category.
+router.post("/category/:id/delete", categoryController.category_delete_post);
+
+// GET request for viewing a category.
+router.get("/category/:id", categoryController.category_detail);
+
+/// ITEM ROUTES ///
 
 // GET items list.
 router.get("/item", itemController.item_list);
